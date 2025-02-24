3db8484f9c4ac47eefbaeb55ea3ba1773e48b457
aumetov <aumetov7@gmail.com>
2025-02-24 10:36:40 +0600

    Initial Commit

---
+++ vpn-client/vpn_clientApp.swift
@@ -0,0 +1,17 @@
+//
+//  vpn_clientApp.swift
+//  vpn-client
+//
+//  Created by Akbar Umetov on 24/2/25.
+//
+
+import SwiftUI
+
+@main
+struct vpn_clientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
