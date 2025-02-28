c617d90a64b1d011c019b8c0b6a65dc754404668
aumetov <aumetov7@gmail.com>
2025-02-28 21:32:08 +0600

    Update VLESSConfig

--- vpn-client/VLESSConfig.swift
+++ vpn-client/VLESSConfig.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import Darwin
 
 struct VLESSConfig {
     let uuid: String
@@ -23,12 +24,10 @@ struct VLESSConfig {
             print("Неверный формат VLESS-ссылки")
             return nil
         }
-        
         guard let components = URLComponents(url: url, resolvingAgainstBaseURL: false) else {
             print("Ошибка разбора URL")
             return nil
         }
-        
         self.uuid = url.user ?? ""
         self.serverAddress = url.host ?? ""
         self.port = url.port ?? 443
@@ -45,50 +44,92 @@ struct VLESSConfig {
 
 extension VLESSConfig {
     func toJSON() -> String? {
-        var config: [String: Any] = [
-            "outbounds": [
-                [
-                    "protocol": "vless",
-                    "settings": [
-                        "vnext": [
-                            [
-                                "address": serverAddress,
-                                "port": port,
-                                "users": [
-                                    [
-                                        "id": uuid,
-                                        "encryption": encryption ?? "none",
-                                        "level": 0,
-                                        "security": security ?? "none"
-                                    ]
-                                ]
-                            ]
-                        ]
-                    ],
-                    "streamSettings": [
-                        "network": type ?? "ws",
-                        "wsSettings": [
-                            "path": path ?? "/"
-                        ]
+        var user: [String: Any] = [
+            "id": uuid,
+            "encryption": encryption ?? "none",
+            "level": 0,
+            "security": security ?? "none"
+        ]
+        
+        if security?.lowercased() == "tls" {
+            let sni = host ?? serverAddress
+            user["tlsSettings"] = [
+                "serverName": sni
+            ]
+        }
+        
+        let outbound: [String: Any] = [
+            "protocol": "vless",
+            "settings": [
+                "vnext": [
+                    [
+                        "address": serverAddress,
+                        "port": port,
+                        "users": [ user ]
                     ]
                 ]
+            ],
+            "streamSettings": [
+                "network": type ?? "ws",
+                "wsSettings": [
+                    "path": path ?? "/"
+                ]
             ]
+        ]
+        
+        var config: [String: Any] = [
+            "outbounds": [ outbound ]
         ]
         
         if let tag = tag {
             config["tag"] = tag
         }
         
-        if let jsonData = try? JSONSerialization.data(withJSONObject: config, options: .prettyPrinted),
-           let jsonString = String(data: jsonData, encoding: .utf8) {
-            return jsonString
+        if
+            let jsonData = try? JSONSerialization.data(withJSONObject: config, options: .prettyPrinted),
+            let jsonString = String(data: jsonData, encoding: .utf8) {
+            return jsonString.replacingOccurrences(of: "\\/", with: "/")
         }
         
+        return nil
+    }
+    
+    func resolveIPAddress() -> String? {
+        var hints = addrinfo(
+            ai_flags: AI_PASSIVE,
+            ai_family: AF_INET,
+            ai_socktype: SOCK_STREAM,
+            ai_protocol: IPPROTO_TCP,
+            ai_addrlen: 0,
+            ai_canonname: nil,
+            ai_addr: nil,
+            ai_next: nil)
+        
+        var info: UnsafeMutablePointer<addrinfo>?
+        let status = getaddrinfo(serverAddress, nil, &hints, &info)
+        guard status == 0, let result = info else {
+            return nil
+        }
+        defer { freeaddrinfo(result) }
+        
+        if let addr = result.pointee.ai_addr {
+            var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
+            let error = getnameinfo(
+                addr, socklen_t(result.pointee.ai_addrlen),
+                &hostname, socklen_t(hostname.count),
+                nil, 0, NI_NUMERICHOST
+            )
+            
+            if error == 0 {
+                return String(cString: hostname)
+            }
+        }
         return nil
     }
     
     enum VLESSError: Error {
         case parsingError
         case jsonError
+        case ipError
     }
 }
